86b5f2f7380e5b92297c4deafbd8f5f33db8a874
pav17 <pav17@hampshire.edu>
2019-02-04 23:07:07 -0500

    Added time out for help command, and disabled command not recognized message.

--- HerdingChats/Assets/Scripts/TwitchClient.cs
+++ HerdingChats/Assets/Scripts/TwitchClient.cs
@@ -10,6 +10,8 @@ public class TwitchClient : MonoBehaviour
     public Client client;
     public string channelName = "sulu244";
     public GameObject CatControllerObject;
+    private float helpTimer;
+    private bool helpFlag;
 
     private CatController catController;
     // Start is called before the first frame update
@@ -28,22 +30,34 @@ public class TwitchClient : MonoBehaviour
         client.SendMessage(client.JoinedChannels[0], "MrTwitchBoto has joined chat");
 
         catController = CatControllerObject.GetComponent<CatController>();
+        helpTimer = 40.0f;
     }
 
     private void CommandListen(object sender, TwitchLib.Client.Events.OnChatCommandReceivedArgs e)
     {
-        if (e.Command.CommandText == "help")
+        if (e.Command.CommandText == "help" && helpFlag == false)
         {
             Help();
+            helpFlag = true;
         }
         else if (e.Command.CommandText == "Up" || e.Command.CommandText == "up" || e.Command.CommandText == "Down" || e.Command.CommandText == "down"
             || e.Command.CommandText == "Left" || e.Command.CommandText == "left" || e.Command.CommandText == "Right" || e.Command.CommandText == "right")
         {
             catController.ChatMoveCommand(e.Command.CommandText);
         }
-        else
+        /*else
         {
             client.SendMessage(client.JoinedChannels[0],"Command not recognized, please use !Help to get the list of commands.");
+        }*/
+
+        if (helpFlag == true)
+        {
+            helpTimer = helpTimer - Time.deltaTime;
+            if (helpTimer <= 0.0f)
+            {
+                helpFlag = false;
+                helpTimer = 40.0f;
+            }
         }
     }
 
