3d24ad81a9bf47c8f538c3085cbe1179543aef09
pav17 <pav17@hampshire.edu>
2019-02-06 15:18:48 -0500

    Cats move if on screen, if none on screen random cat moves.

--- HerdingChats/Assets/Scripts/CatController.cs
+++ HerdingChats/Assets/Scripts/CatController.cs
@@ -19,20 +19,29 @@ public class CatController : MonoBehaviour
         GameObject[] cats;
         int r;
         cats = GameObject.FindGameObjectsWithTag("Cat");
-        r = Random.Range(0, cats.Length);
-        selectedCat = cats[r];
-        selectedCat.GetComponent<Cat>().Move(direction);
+       
 
        List<GameObject> catsOnCamera = new List<GameObject>();
 
-        for (int i = 0; i <= cats.Length; i++)
+        for (int i = 0; i < cats.Length; i++)
         {
             if (cats[i].transform.position.x > camera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x && cats[i].transform.position.x < camera.ViewportToWorldPoint(new Vector3(1, 1, 0)).x)
                 if (cats[i].transform.position.y > camera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y && cats[i].transform.position.y < camera.ViewportToWorldPoint(new Vector3(1, 1, 0)).y)
                     catsOnCamera.Add(cats[i]);
         }
-
-        print(catsOnCamera.Count);
-
+        if(catsOnCamera.Count == 0)
+        {
+            r = Random.Range(0, cats.Length);
+            selectedCat = cats[r];
+            selectedCat.GetComponent<Cat>().Move(direction);
+            //Debug.Log("Cat " + selectedCat + " Moved");
+        }
+        else
+        {
+            r = Random.Range(0, catsOnCamera.Count);
+            selectedCat = catsOnCamera[r];
+            selectedCat.GetComponent<Cat>().Move(direction);
+            //Debug.Log("Cat " + selectedCat + " Moved");
+        }
     }
 }
