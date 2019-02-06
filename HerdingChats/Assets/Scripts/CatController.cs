ff8880136a4f642b28507cc7eebb792975e9e699
pav17 <pav17@hampshire.edu>
2019-02-06 09:57:42 -0500

    Reverted changes, wasn't working, needs a closer look.

--- HerdingChats/Assets/Scripts/CatController.cs
+++ HerdingChats/Assets/Scripts/CatController.cs
@@ -18,32 +18,35 @@ public class CatController : MonoBehaviour
         GameObject[] cats;
         int r;
         cats = GameObject.FindGameObjectsWithTag("Cat");
-        GameObject[] catsOnCamera = new GameObject[cats.Length];
-
-        for (int i = 0; i <= cats.Length; i++)
-        {
-            if (cats[i].transform.position.x > camera.ViewportToWorldPoint(new Vector3(0,0,0)).x && cats[i].transform.position.x < camera.ViewportToWorldPoint(new Vector3(1,1,0)).x)
-            {
-                if (cats[i].transform.position.y > camera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y && cats[i].transform.position.y < camera.ViewportToWorldPoint(new Vector3(1, 1, 0)).y)
+        r = Random.Range(0, cats.Length);
+        selectedCat = cats[r];
+        selectedCat.GetComponent<Cat>().Move(direction);
+        //GameObject[] catsOnCamera = new GameObject[cats.Length];
+        /*
+                for (int i = 0; i <= cats.Length; i++)
                 {
-                    catsOnCamera[i] = cats[i];
+                    if (cats[i].transform.position.x > camera.ViewportToWorldPoint(new Vector3(0,0,0)).x && cats[i].transform.position.x < camera.ViewportToWorldPoint(new Vector3(1,1,0)).x)
+                    {
+                        if (cats[i].transform.position.y > camera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y && cats[i].transform.position.y < camera.ViewportToWorldPoint(new Vector3(1, 1, 0)).y)
+                        {
+                            catsOnCamera[i] = cats[i];
+                        }
+                    }
                 }
-            }
-        }
 
-        for (int i = 0; i <= catsOnCamera.Length; i++)
-        {
-            r = Random.Range(0, cats.Length);
-            if (catsOnCamera[r] == null)
-            {
-                continue;
-            }
-            else
-            {
-                selectedCat = catsOnCamera[r];
-                selectedCat.GetComponent<Cat>().Move(direction);
-                break;
-            }
-        }
+                for (int i = 0; i <= catsOnCamera.Length; i++)
+                {
+                    r = Random.Range(0, cats.Length);
+                    if (catsOnCamera[r] == null)
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        selectedCat = catsOnCamera[r];
+                        selectedCat.GetComponent<Cat>().Move(direction);
+                        break;
+                    }
+                }*/
     }
 }
