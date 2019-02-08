881e6acaac73378777d1710f6759c39cbee582c1
pav17 <pav17@hampshire.edu>
2019-02-08 14:08:58 -0500

    Added main menu and level select

---
+++ HerdingChats/Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    public void LevelSelect()
+    {
+        SceneManager.LoadScene(sceneName: "LevelSelect");
+    }
+
+    public void Options()
+    {
+        SceneManager.LoadScene(sceneName: "Options");
+    }
+}
