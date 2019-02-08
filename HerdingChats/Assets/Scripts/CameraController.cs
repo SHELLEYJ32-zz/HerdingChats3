a9559a03833c585e5d687b7fed0173493990f87f
SHELLEYJ32 <jiang25s@mtholyoke.edu>
2019-02-08 15:00:35 -0500

    camera controller change

--- HerdingChats/Assets/Scripts/CameraController.cs
+++ HerdingChats/Assets/Scripts/CameraController.cs
@@ -3,29 +3,16 @@ using System.Collections;
 
 public class CameraController : MonoBehaviour
 {
-    public GameObject player, background;
+    public GameObject player;
 
     void Start()
     {
-        transform.position = new Vector3(background.transform.position.x, background.transform.position.y, 0);
+        transform.position = new Vector3(player.transform.position.x, player.transform.position.y, 0);
     }
 
     void FixedUpdate()
     {
-
-        if (Approximately(transform.position.x, player.transform.position.x, 0.1f))
-        {
-            transform.position = new Vector3(player.transform.position.x, transform.position.y, 0);
-        }
-        if (Approximately(transform.position.y, player.transform.position.y, 0.1f))
-        {
-            transform.position = new Vector3(transform.position.x, player.transform.position.y, 0);
-        }
-
+        transform.position = new Vector3(player.transform.position.x, player.transform.position.y, 0);
     }
 
-    bool Approximately(float a, float b, float threshold)
-    {
-        return System.Math.Abs(a - b) <= threshold;
-    }
 }
