e0b7c16fd7fc7743ef14275fd836d2c7828edcf4
SHELLEYJ32 <jiang25s@mtholyoke.edu>
2019-02-22 15:51:44 -0500

    attempted fix of sorting layer

--- HerdingChats/Assets/Scripts/General/SpriteRenderOrderController.cs
+++ HerdingChats/Assets/Scripts/General/SpriteRenderOrderController.cs
@@ -1,18 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class SpriteRenderOrderController : MonoBehaviour
 {
 
     void FixedUpdate()
     {
-        SpriteRenderer[] renderers = FindObjectsOfType<SpriteRenderer>();
+        SpriteRenderer[] spriteRenderers = FindObjectsOfType<SpriteRenderer>();
+        TilemapRenderer tilemapRenderers = FindObjectOfType<TilemapRenderer>();
 
-        foreach (SpriteRenderer renderer in renderers)
+        foreach (SpriteRenderer renderer in spriteRenderers)
         {
             renderer.sortingOrder = (int)(renderer.transform.position.y * -10);
         }
+
+        tilemapRenderers.sortingOrder = (int)(tilemapRenderers.transform.position.y * -10);
 
     }
 }
