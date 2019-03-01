65a9cc09a0b00849843f3d19cd9ede2dc79e7ed1
SHELLEYJ32 <jiang25s@mtholyoke.edu>
2019-03-01 13:57:27 -0500

    script and scene cleaning, comment for CameraController

--- HerdingChats/Assets/Scripts/General/CameraController.cs
+++ HerdingChats/Assets/Scripts/General/CameraController.cs
@@ -1,15 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+
 public class CameraController : MonoBehaviour
 {
     public GameObject player;
 
+    //camera centers the player
     void Start()
     {
         transform.position = new Vector3(player.transform.position.x, player.transform.position.y, 0);
     }
 
+    //camera stays with the player
     void FixedUpdate()
     {
         transform.position = new Vector3(player.transform.position.x, player.transform.position.y, 0);
